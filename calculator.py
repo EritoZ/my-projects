80c23b6c66204c702060f3047956d0af7a2a6467
EritoZ <moni.grozdev@gmail.com>
2023-03-04 01:53:47 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -7,7 +7,7 @@ def get_first_num(sub_or_full_expression, index):
     start = 0
 
     for i in range(index - 1, -1, -1):
-        if sub_or_full_expression[i] != " " and sub_or_full_expression[i] != "(":
+        if sub_or_full_expression[i] != " ":
             num += sub_or_full_expression[i]
             if i == 0:
                 start = i
@@ -23,7 +23,7 @@ def get_second_num(sub_or_full_expression, index):
     end = 0
 
     for i in range(index + 3, len(sub_or_full_expression)):
-        if sub_or_full_expression[i] != " " and sub_or_full_expression[i] != ")":
+        if sub_or_full_expression[i] != " ":
             num += sub_or_full_expression[i]
             if i == len(sub_or_full_expression) - 1:
                 end = i + 1
@@ -37,10 +37,10 @@ def get_second_num(sub_or_full_expression, index):
 def addition(sub_or_full_expression):
     while " + " in sub_or_full_expression:
         plus_location = sub_or_full_expression.index(" + ")
-        
+
         first_num, start = get_first_num(sub_or_full_expression, plus_location)
         second_num, end = get_second_num(sub_or_full_expression, plus_location)
-        
+
         result = str(first_num + second_num)
 
         sub_or_full_expression = sub_or_full_expression[:start] + result + sub_or_full_expression[end:]
@@ -51,10 +51,10 @@ def addition(sub_or_full_expression):
 def subtraction(sub_or_full_expression):
     while " - " in sub_or_full_expression:
         minus_location = sub_or_full_expression.index(" - ")
-        
+
         first_num, start = get_first_num(sub_or_full_expression, minus_location)
         second_num, end = get_second_num(sub_or_full_expression, minus_location)
-        
+
         result = str(first_num - second_num)
 
         sub_or_full_expression = sub_or_full_expression[:start] + result + sub_or_full_expression[end:]
@@ -65,10 +65,10 @@ def subtraction(sub_or_full_expression):
 def multiplication(sub_or_full_expression):
     while " * " in sub_or_full_expression:
         multiplication_location = sub_or_full_expression.index(" * ")
-        
+
         first_num, start = get_first_num(sub_or_full_expression, multiplication_location)
         second_num, end = get_second_num(sub_or_full_expression, multiplication_location)
-        
+
         result = str(first_num * second_num)
 
         sub_or_full_expression = sub_or_full_expression[:start] + result + sub_or_full_expression[end:]
@@ -79,10 +79,10 @@ def multiplication(sub_or_full_expression):
 def division(sub_or_full_expression):
     while " / " in sub_or_full_expression:
         division_location = sub_or_full_expression.index(" / ")
-        
+
         first_num, start = get_first_num(sub_or_full_expression, division_location)
         second_num, end = get_second_num(sub_or_full_expression, division_location)
-        
+
         result = str(first_num / second_num)
 
         sub_or_full_expression = sub_or_full_expression[:start] + result + sub_or_full_expression[end:]
@@ -95,10 +95,10 @@ def bracket_solving(the_expression):
 
         if "(" in the_expression and ")" in the_expression:
             bracket_expression = re.search(r"\(([^(]+?)\)", the_expression)
-            
+
             bracket_expression_start = bracket_expression.start()
             bracket_expression_end = bracket_expression.end()
-            
+
             bracket_expression = bracket_expression.group(1)
 
             bracket_expression = multiplication(bracket_expression)
