b6f1b666e9306d6bddc9bd26a03813a6d1107c50
Simeon Grozdev <moni.grozdev@gmail.com>
2024-01-04 04:32:02 +0200

    Rewrite part of time stopper code.

--- time_stopper.py
+++ time_stopper.py
@@ -8,7 +8,7 @@ class TimeStopper:
         self.root = tk.Tk()
 
         self.root.geometry('500x400')
-        self.root.title('Clock Stopper')
+        self.root.title('Time Stopper')
 
         self.clock = tk.Label(self.root)
         self.clock.pack(pady=100)
@@ -16,7 +16,7 @@ class TimeStopper:
         self.button_pause_or_start = tk.Button(self.root,
                                                text='Pause/Start',
                                                font=('Arial', 15),
-                                               command=self.toggle_pause,)
+                                               command=self.toggle_pause)
         self.button_pause_or_start.pack()
 
         self.button_reset = tk.Button(self.root,
@@ -32,37 +32,29 @@ class TimeStopper:
 
         self.root.mainloop()
 
-    def create_timedelta_from_now(self):
-        now = datetime.datetime.now()
-
-        return datetime.timedelta(hours=now.hour, minutes=now.minute,
-                                  seconds=now.second, milliseconds=now.microsecond // 1000)
-
     def update_time(self):
-
         if not self.paused:
-            time = self.create_timedelta_from_now()
+            time = datetime.datetime.now()
 
             time -= self.time_difference
 
-            self.clock.config(text=str(time)[:-3], font=('Arial', 20))
+            self.clock.config(text=time.strftime('%H:%M:%S:%f')[:-3], font=('Arial', 20))
 
         self.root.after(10, self.update_time)
 
     def toggle_pause(self):
         if not self.paused:
             self.paused = True
-            self.paused_time = self.create_timedelta_from_now()
+            self.paused_time = datetime.datetime.now()
 
             return
 
-        self.time_difference += self.create_timedelta_from_now() - self.paused_time
+        self.time_difference += datetime.datetime.now() - self.paused_time
         self.paused = False
 
     def reset_time(self):
         self.time_difference = datetime.timedelta(hours=0, minutes=0, seconds=0, milliseconds=0)
-        self.paused_time = self.create_timedelta_from_now()
-        self.clock.config(text=str(self.paused_time)[:-3], font=('Arial', 20))
+        self.paused = False
 
 
 TimeStopper()
